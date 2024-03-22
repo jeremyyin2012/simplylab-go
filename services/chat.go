19b2278294dcdd25c7c98c4d965a5f7adf11ee28
Jeremy Yin <jeremyyin2012@gmail.com>
2024-03-22 22:39:24 +0800

    chore: define svc func

--- services/chat.go
+++ services/chat.go
@@ -13,3 +13,14 @@ type ChatService struct {
 func (s ChatService) GetAIChatResponse(req model.GetAiChatResponseInput) model.GetAiChatResponseOutput {
 	return model.GetAiChatResponseOutput{Response: "todo"}
 }
+
+func (s ChatService) GetUserChatHistory(lastN int) model.GetUserChatHistoryOutput {
+	return make(model.GetUserChatHistoryOutput, 0)
+}
+
+func (s ChatService) GetChatStatusToday() model.GetChatStatusTodayOutput {
+	return model.GetChatStatusTodayOutput{
+		UserName: s.ctx.User.Name,
+		ChatCnt:  0,
+	}
+}
