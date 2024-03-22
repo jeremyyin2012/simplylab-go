7578dd837d7e831a38cc7c7d9d8d21565ee33630
Jeremy Yin <jeremyyin2012@gmail.com>
2024-03-22 22:36:25 +0800

    chore: define provider fun, not error include yet

--- providers/chat.go
+++ providers/chat.go
@@ -1,5 +1,26 @@
 package providers
 
+import "simplylab/model"
+
 type ChatProvider struct {
-	
+}
+
+func (p ChatProvider) CheckUserMessageLimitedIn30Seconds(user model.User) bool {
+	return false
+}
+
+func (p ChatProvider) CheckUserMessageLimitedInDaily(user model.User) bool {
+	return false
+}
+
+func (p ChatProvider) AddChatMessages(messages []model.NewMessage) int {
+	return 0
+}
+
+func (p ChatProvider) GetUserChatMessages(user model.User, limit int) []model.Message {
+	return make([]model.Message, 0)
+}
+
+func (p ChatProvider) GetUserChatMessagesCountToday(user model.User) int {
+	return 0
 }
--- providers/openrouter.go
+++ providers/openrouter.go
@@ -3,3 +3,7 @@ package providers
 type OpenRouterProvider struct {
 
 }
+
+func (p OpenRouterProvider) Chat(content string) string {
+	return "todo"
+}
